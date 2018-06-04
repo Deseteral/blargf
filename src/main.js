fe8006ee4e8975477c4a9db3000d5920899425b8
Deseteral <deseteral@gmail.com>
2018-06-04 18:03:46 +0200

    gzip responses

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const compression = require('compression');
 const app = express();
 const getTasks = require('./tasks');
 
 const { PORT } = process.env;
 
+app.use(compression());
 app.use('/', express.static(`${__dirname}/static`));
 app.get('/tasks', async (req, res) => {
   const tasks = await getTasks();
