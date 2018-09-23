2f231b234644e364b3f9856b0683574f63c310a6
Deseteral <deseteral@gmail.com>
2018-09-23 16:49:30 +0200

    Don't read iCal urls from env every on fetch

--- src/services/ical.js
+++ src/services/ical.js
@@ -1,9 +1,7 @@
 const ical = require('node-ical');
 const flatten = require('array-flatten');
 
-function getUrlsFromConfiguration() {
-  return JSON.parse(process.env.EVENTS_ICAL_URLS);
-}
+const ICAL_SOURCE_URLS = JSON.parse(process.env.EVENTS_ICAL_URLS);
 
 function fetchICalFromUrl(url) {
   return new Promise((resolve, reject) => {
@@ -23,9 +21,7 @@ function fetchICalFromUrl(url) {
 }
 
 async function fetchICalEvents() {
-  const urls = getUrlsFromConfiguration();
-
-  return flatten(await Promise.all(urls.map(fetchICalFromUrl)));
+  return flatten(await Promise.all(ICAL_SOURCE_URLS.map(fetchICalFromUrl)));
 }
 
 module.exports = { fetchICalEvents };
