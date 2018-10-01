54b5a43e755c2bccd6101b31129f0d5bc7fe2ce3
Deseteral <deseteral@gmail.com>
2018-10-02 00:04:43 +0200

    Gulp watch restarts application after changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const del = require('del');
 const extname = require('gulp-extname');
+const { spawn } = require('child_process');
 
 gulp.task('clean', () => del(['build']));
 
@@ -22,6 +23,23 @@ gulp.task('default', gulp.series(
   gulp.parallel('js', 'css'),
 ));
 
-gulp.task('dev', () => (
-  gulp.watch('src/**/*', gulp.series('default'))
-));
+let node = null;
+function runApplication(cb) {
+  if (node) node.kill();
+
+  node = spawn('node', ['build/main.js'], { stdio: 'inherit' });
+  node.on('close', (exitCode) => {
+    if (exitCode === 8) gulp.log('Error detected, waiting for changes...');
+  });
+
+  if (cb) cb();
+}
+
+gulp.task('dev', () => {
+  runApplication();
+  gulp.watch('src/**/*', gulp.series('default', runApplication));
+});
+
+process.on('exit', () => {
+  if (node) node.kill();
+});
