67c6036379a72c0220899f8e2e9ed3ae9e3fd8c4
Deseteral <deseteral@gmail.com>
2018-10-28 12:58:57 +0100

    Tasks components styling

--- src/components/TasksSection.jsx
+++ src/components/TasksSection.jsx
@@ -1,10 +1,44 @@
 import React, { Fragment } from 'react';
+import styled from 'react-emotion';
 import Card from './atomic/Card';
 import CardHeader from './atomic/CardHeader';
 import Caption from './atomic/Caption';
 import Icon from './atomic/Icon';
 import TaskCountLabel from './TaskCountLabel';
-import TaskListElement from './TaskListElement';
+
+const TaskList = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
+
+const EmptyTaskListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const EmptyTaskListFlag = styled(Icon)`
+  margin-bottom: 8px;
+  font-size: 48px;
+  opacity: 0.56;
+`;
+EmptyTaskListFlag.defaultProps = { type: 'outlined_flag' };
+
+const TaskListElement = styled.li`
+  display: flex;
+  align-items: center;
+  min-height: 48px;
+  list-style: none;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+`;
+
+const IncompleteTaskIcon = styled(Icon)`
+  margin-right: 8px;
+`;
+IncompleteTaskIcon.defaultProps = { type: 'radio_button_unchecked' };
 
 function TasksSection({ tasks }) {
   const { list, lastUpdateFailed } = tasks;
@@ -14,17 +48,21 @@ function TasksSection({ tasks }) {
     <Card>
       <CardHeader>Tasks</CardHeader>
       {isEmpty && (
-        <div className="no-tasks-container">
-          <Icon type="outlined_flag" className="no-tasks-flag" />
+        <EmptyTaskListContainer>
+          <EmptyTaskListFlag />
           <Caption>There are no tasks planned for today.</Caption>
-        </div>
+        </EmptyTaskListContainer>
       )}
       {!isEmpty && (
         <Fragment>
           <TaskCountLabel count={list.length} />
-          <ul className="list">
-            {list.map((t, idx) => <TaskListElement content={t.content} key={idx} />)}
-          </ul>
+          <TaskList>
+            {list.map((t, idx) => (
+              <TaskListElement key={idx}>
+                <IncompleteTaskIcon /> {t.content}
+              </TaskListElement>
+            ))}
+          </TaskList>
         </Fragment>
       )}
       {lastUpdateFailed && (
