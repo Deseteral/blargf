e1758a1e4c1e6f96b7046caa60c7a52b1bcd2cc7
Deseteral <deseteral@gmail.com>
2020-07-04 16:21:32 +0200

    Fresh look

--- src/components/atomic/Card.jsx
+++ src/components/atomic/Card.jsx
@@ -7,7 +7,7 @@ const Card = styled.section`
   background-color: #FFFFFF;
   padding: 16px;
   margin-top: 16px;
-  border-radius: 2px;
+  border-radius: 8px;
   box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
   transition: all 0.5s ease;
   opacity: 1;
