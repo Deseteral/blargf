dd0c6cc8c8d347fa6df796e7a297f0469d863ed1
Deseteral <deseteral@gmail.com>
2025-06-01 01:12:04 +0200

    Remove Things integration

--- src/domain/tasks/tasks.ts
+++ src/domain/tasks/tasks.ts
@@ -1,25 +1,21 @@
-import { exec } from 'child_process';
-import { nanoid } from 'nanoid';
-import registerService, { DataCache } from '../../services/register-service';
-import config from '../../config';
-import { TaskList, Task } from './model';
-// @ts-ignore
-import readTodayTasksScript from './read_today_tasks.applescript';
+import registerService, { DataCache } from "../../services/register-service";
+import config from "../../config";
+import { TaskList, Task } from "./model";
 
 type TodoistResponse = TodoistTask[];
 
 interface TodoistTask {
-  id: string,
-  content: string,
+  id: string;
+  content: string;
   due?: {
-    date: string,
-  },
+    date: string;
+  };
 }
 
 async function fetchTodoistTasksDueToday(): Promise<TaskList> {
-  const TASKS_URL = 'https://api.todoist.com/rest/v2/tasks';
+  const TASKS_URL = "https://api.todoist.com/rest/v2/tasks";
 
-  const currentDate = new Date().toISOString().split('T')[0];
+  const currentDate = new Date().toISOString().split("T")[0];
   const token = config().tasks.todoistToken;
   const fetchOptions = {
     headers: {
@@ -28,7 +24,7 @@ async function fetchTodoistTasksDueToday(): Promise<TaskList> {
   };
 
   const res = await fetch(TASKS_URL, fetchOptions);
-  const tasks = await res.json() as TodoistResponse;
+  const tasks = (await res.json()) as TodoistResponse;
 
   const todaysTasks: Task[] = tasks
     .filter((task) => task.due && task.due.date === currentDate)
@@ -44,42 +40,12 @@ async function fetchTodoistTasksDueToday(): Promise<TaskList> {
   };
 }
 
-function execAppleScript(scriptPath: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    exec(`osascript ${scriptPath}`, (error, _stdout, stderr) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      resolve(stderr);
-    });
-  });
-}
-
-// @ts-ignore
-async function readTodayTasksFromThings(): Promise<TaskList> { // eslint-disable-line no-unused-vars
-  const scriptPath = new URL(readTodayTasksScript).pathname;
-  const stdout = await execAppleScript(scriptPath);
-
-  const today = stdout
-    .split('\n')
-    .filter((s) => (s.length > 0))
-    .map((s) => ({
-      id: nanoid(),
-      content: s,
-    }));
-
-  return {
-    today,
-  };
-}
-
 const [getTasks] = registerService<TaskList, DataCache<TaskList>>({
-  name: 'tasks',
+  name: "tasks",
   refreshInterval: config().tasks.refreshIntervalSeconds,
   dataProvider: fetchTodoistTasksDueToday,
   initialData: { today: [] },
-  getter: (cache, _refreshCache) => cache,
+  getter: (cache) => cache,
 });
 
 export { getTasks };
